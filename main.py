7d2bb9003384aad4e3c9cefe5279af66028e814c
GrantSRechtin <gsullivanr@icloud.com>
2025-11-06 15:14:54 -0500

    name changes and code improvements

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import cv2
 import time
 import numpy as np
 import sympy
-import math
+import copy
 import transform_eyrie_board
+import roost_detection_test
 from operator import itemgetter
 from itertools import combinations
 
 
-class testing:
+class player_board:
 
     def __init__(self):
 
         self.active = True
         self.cv_image = None
+        self.bt = False
+
+        self.current_image = None
+
+        self.oriented = False
 
         self.i = 0
 
@@ -26,8 +32,7 @@ class testing:
 
         path = "52B-RootBaseFactionBoardwithComponents-Editv2-web.webp"
         self.img = cv2.imread(path)
-
-        self.img = self.blur(self.img)
+        self.img = blur(self.img)
 
         # Card Slots
         # self.rl = 0
@@ -44,116 +49,82 @@ class testing:
         self.birdsong_init = (0, 155, 196, 82, 196, 240)
 
         # Main
-        # self.rl = 70
-        # self.rh = 150
-        # self.gl = 140
-        # self.gh = 200
-        # self.bl = 150
-        # self.bh = 240
+        self.binary_calibration = (70, 140, 150, 150, 200, 240)
 
         if self.vc.isOpened():
             self.cv_image = self.img  # self.vc.read()
+            self.current_image = copy.copy(self.cv_image)
 
     def loop_wrapper(self):
         """loops run_loop"""
 
         cv2.namedWindow("video_window", cv2.WINDOW_NORMAL)
-        # cv2.namedWindow("binary_window")
-        # cv2.createTrackbar(
-        #     "red lower bound", "binary_window", self.rl, 255, self.set_rl
-        # )
-        # cv2.createTrackbar(
-        #     "red upper bound", "binary_window", self.rh, 255, self.set_rh
-        # )
-        # cv2.createTrackbar(
-        #     "green lower bound", "binary_window", self.gl, 255, self.set_gl
-        # )
-        # cv2.createTrackbar(
-        #     "green upper bound", "binary_window", self.gh, 255, self.set_gh
-        # )
-        # cv2.createTrackbar(
-        #     "blue lower bound", "binary_window", self.bl, 255, self.set_bl
-        # )
-        # cv2.createTrackbar(
-        #     "blue upper bound", "binary_window", self.bh, 255, self.set_bh
-        # )
+        #self.binary_testing()
 
         while self.active:
             self.run_loop()
             time.sleep(0.1)
 
     def run_loop(self):
+        if not self.current_image is None:
 
-        if not self.cv_image is None:
+            cv2.imshow("video_window", self.current_image)
 
-            # Get initial binary image
-            self.binary_image = cv2.inRange(
-                self.cv_image, self.birdsong_init[0:3], self.birdsong_init[3:6]
+            # binary testing window
+            if (self.bt):
+                self.binary_image = cv2.inRange(
+                    self.cv_image, (self.binary_calibration[0], self.binary_calibration[1], self.binary_calibration[2]), (self.binary_calibration[3], self.binary_calibration[4], self.binary_calibration[5]))
+                cv2.imshow('binary_window', self.binary_image)
+
+
+            key = cv2.waitKey(20)
+            if key == 27:  # Escape
+                self.esc()
+            elif key == 32: # Space
+
+                if not self.oriented:
+                    self.board_orientation()
+                else:
+                    self.update_board()
+
+    def update_board(self):
+        contours = roost_detection_test.detect_roosts(self)
+        print(contours)
+        for i in enumerate(contours):
+            cv2.drawContours(
+                self.current_image,
+                contours,
+                i[0],
+                (0, 0, 255),
+                1,
             )
-            # self.binary_image = cv2.inRange(
-            #     self.cv_image, (self.rl, self.gl, self.bl), (self.rh, self.gh, self.bh))
 
-            cv2.imshow("video_window", self.cv_image)
-            # cv2.imshow('binary_window', self.binary_image)
+    def esc(self):
+        cv2.destroyAllWindows()
+        self.vc.release()
+        self.active = False
 
-            self.check_input()
+    def board_orientation(self):
 
-    def check_input(self):
-        key = cv2.waitKey(20)
-        if key == 27:
-            cv2.destroyAllWindows()
-            self.vc.release()
-            self.active = False
-        elif key == 32:
-            contours = self.create_binary_contours(self.binary_image)
-            pts = self.find_birdsong_border(contours[0][1], self.birdsong)
-            print(pts)
-            # Br Bl Tl Tr
-            transformed_corners = [(204.4, 276), (46, 276), (46, 258), (204.4, 258)]
-            cv2.namedWindow("Original image")
-            cv2.namedWindow("Transformed image")
-            cv2.imshow("Original image", self.cv_image)
-            trans_image = transform_eyrie_board.transform_board(
-                pts, transformed_corners, self.cv_image
-            )
-            cv2.imshow("Transformed image", trans_image)
-
-    def blur(self, img):
-        h, w, channels = img.shape
-        return cv2.resize(
-            cv2.resize(img, (700, 550), interpolation=cv2.INTER_AREA), (w, h)
+        bs_binary = cv2.inRange(
+            self.cv_image, self.birdsong_init[0:3], self.birdsong_init[3:6]
         )
 
-    def create_binary_contours(self, binary_image, invert=False):
-
-        if invert:
-            binary_image = cv2.bitwise_not(binary_image)
-
-        contour_list, hierarchy = cv2.findContours(
-            binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE
+        contours = create_binary_contours(bs_binary)
+        pts = self.find_birdsong_border(contours[0][1], self.birdsong)
+        print(pts)
+        transformed_corners = [
+            (204.4, 276), (46, 276), (46, 258), (204.4, 258)]
+        self.translated_image = transform_eyrie_board.transform_board(
+            pts, transformed_corners, self.cv_image
         )
-        contour_areas = [cv2.contourArea(c) for c in contour_list]
-        contours = [
-            (contour_areas[i], contour_list[i]) for i in range(len(contour_list))
-        ]
-        contours = sorted(contours, key=itemgetter(0))[::-1]
 
-        return contours
+        cv2.namedWindow("Original image")
+        cv2.imshow("Original image", self.cv_image)
 
-    def create_contours(self, image):
+        self.current_image = copy.copy(self.translated_image)
 
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        edged = cv2.Canny(gray, 30, 200)
-        contour_list, hierarchy = cv2.findContours(
-            edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-        )
-        contour_areas = [cv2.contourArea(c) for c in contour_list]
-        contours = [
-            (contour_areas[i], contour_list[i]) for i in range(len(contour_list))
-        ]
-        contours = sorted(contours, key=itemgetter(0))[::-1]
-
-        return contours
+        self.oriented = True
 
     def find_birdsong_border(self, contour, c_range):
         xs = [c[0][0] for c in contour]
@@ -183,24 +154,19 @@ class testing:
             (c_range[3], c_range[4], c_range[5]),
         )
 
+        #map_to_list(binary_image)
+
         birdsong_region = cv2.bitwise_and(binary_image, circle)
-        contours = self.create_binary_contours(birdsong_region)
+        contours = create_binary_contours(birdsong_region)
 
         blank_image = np.zeros((h, w, 3), np.uint8)
         cv2.drawContours(blank_image, [contours[0][1]], 0, (0, 0, 255), 1)
 
         app = appx_best_fit_ngon(blank_image)
         pts = np.array(app, dtype=np.int32)
-        # if pts.ndim == 2 and pts.shape[1] == 2:
-        #     pts = pts.reshape((-1, 1, 2))
 
-        for i in range(3):
-            for pt in pts:
-                pt = move_closest_point_towards(pt, contours[0][1])
-
-        # pts[2][1] += 3
-        # pts[3][1] += 3
-        # pts[3][0] -= 3
+        for i in range(len(pts)):
+            pts[i], _ = move_closest_point_towards(pts[i], contours[0][1],1)
 
         contour = self.cv_image.copy()
         cv2.drawContours(contour, [contours[0][1]], 0, (0, 0, 255), 1)
@@ -216,31 +182,90 @@ class testing:
     def set_border(self, xs, ys):
         print(xs[0], xs[1])
         print(ys[1], ys[0])
-        self.cv_image = self.cv_image[ys[1] : ys[0], xs[0] : xs[1]]
+        self.cv_image = self.cv_image[ys[1]: ys[0], xs[0]: xs[1]]
 
     def set_rl(self, val):
         """A callback function to handle the OpenCV slider to select the red lower bound"""
-        self.rl = val
+        self.binary_calibration[0] = val
 
     def set_rh(self, val):
         """A callback function to handle the OpenCV slider to select the red upper bound"""
-        self.rh = val
+        self.binary_calibration[3] = val
 
     def set_gl(self, val):
         """A callback function to handle the OpenCV slider to select the green lower bound"""
-        self.gl = val
+        self.binary_calibration[1] = val
 
     def set_gh(self, val):
         """A callback function to handle the OpenCV slider to select the green upper bound"""
-        self.gh = val
+        self.binary_calibration[4] = val
 
     def set_bl(self, val):
         """A callback function to handle the OpenCV slider to select the blue lower bound"""
-        self.bl = val
+        self.binary_calibration[2] = val
 
     def set_bh(self, val):
         """A callback function to handle the OpenCV slider to select the blue upper bound"""
-        self.bh = val
+        self.binary_calibration[5] = val
+
+    def binary_testing(self):
+        self.bt = True
+        cv2.namedWindow("binary_window")
+        cv2.createTrackbar(
+            "red lower bound", "binary_window", self.binary_calibration[0], 255, self.set_rl
+        )
+        cv2.createTrackbar(
+            "red upper bound", "binary_window", self.binary_calibration[3], 255, self.set_rh
+        )
+        cv2.createTrackbar(
+            "green lower bound", "binary_window", self.binary_calibration[1], 255, self.set_gl
+        )
+        cv2.createTrackbar(
+            "green upper bound", "binary_window", self.binary_calibration[4], 255, self.set_gh
+        )
+        cv2.createTrackbar(
+            "blue lower bound", "binary_window", self.binary_calibration[2], 255, self.set_bl
+        )
+        cv2.createTrackbar(
+            "blue upper bound", "binary_window", self.binary_calibration[5], 255, self.set_bh
+        )
+
+def blur(img):
+    h, w, channels = img.shape
+    return cv2.resize(
+        cv2.resize(img, (700, 550), interpolation=cv2.INTER_AREA), (w, h)
+    )
+
+def create_binary_contours(binary_image, invert=False):
+
+    if invert:
+        binary_image = cv2.bitwise_not(binary_image)
+
+    contour_list, hierarchy = cv2.findContours(
+        binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE
+    )
+    contour_areas = [cv2.contourArea(c) for c in contour_list]
+    contours = [
+        (contour_areas[i], contour_list[i]) for i in range(len(contour_list))
+    ]
+    contours = sorted(contours, key=itemgetter(0))[::-1]
+
+    return contours
+
+def create_contours(image):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray, 30, 200)
+    contour_list, hierarchy = cv2.findContours(
+        edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
+    contour_areas = [cv2.contourArea(c) for c in contour_list]
+    contours = [
+        (contour_areas[i], contour_list[i]) for i in range(len(contour_list))
+    ]
+    contours = sorted(contours, key=itemgetter(0))[::-1]
+
+    return contours
 
 
 def appx_best_fit_ngon(mask_cv2, n: int = 4) -> list[(int, int)]:
@@ -286,7 +311,8 @@ def appx_best_fit_ngon(mask_cv2, n: int = 4) -> list[(int, int)]:
             intersect = adj_edge_1.intersection(adj_edge_2)[0]
 
             # the area of the triangle we'll be adding
-            area = sympy.N(sympy.Triangle(edge_pt_1, intersect, edge_pt_2).area)
+            area = sympy.N(sympy.Triangle(
+                edge_pt_1, intersect, edge_pt_2).area)
             # should be the lowest
             if best_candidate and best_candidate[1] < area:
                 continue
@@ -307,6 +333,13 @@ def appx_best_fit_ngon(mask_cv2, n: int = 4) -> list[(int, int)]:
 
     return hull
 
+def map_to_list(map):
+    list = np.array()
+    for x in range(len(map)):
+        for y in range(len(map[0])):
+            if map[x][y] == 255:
+                list.append((x,y))
+    return list        
 
 def move_closest_point_towards(target_point, points, step=1.0):
     """Move the target_point a little closer to the nearest point in `points`.
@@ -349,7 +382,7 @@ def move_closest_point_towards(target_point, points, step=1.0):
     if dist == 0 or step == 0:
         return tgt, idx
 
-    move = min(step, dist)
+    move = dist #min(step, dist)
     direction = deltas[idx] / dist
     new_tgt = tgt + direction * move
 
--- roost_detection_test.py
+++ roost_detection_test.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
-from initial_test import testing
 
-
-def detect_roosts(testing_instance):
+def detect_roosts(player_board):
     """
     Given an image of the roosts section of the eyrie player board,
     find roost tokens.
@@ -22,13 +20,13 @@ def detect_roosts(testing_instance):
     bl = 25
     bh = 290
     # Generate binary image
-    binary_image = cv2.inRange(testing_instance.img, (rl, gl, bl), (rh, gh, bh))
+    binary_image = cv2.inRange(player_board.current_image, (rl, gl, bl), (rh, gh, bh))
     # Uncomment to display binary image
     # cv2.namedWindow("Binary image")
     # cv2.imshow("Binary image", binary_image)
 
     # Generate contours
-    contours = testing_instance.create_binary_contours(binary_image)
+    contours = player_board.create_binary_contours(binary_image)
     # Filter contours by area
     roost_contour = []
     for i in contours:
@@ -37,23 +35,23 @@ def detect_roosts(testing_instance):
     return roost_contour
 
 
-if __name__ == "__main__":
-    test_image = testing()
-    while test_image.active:
-        roost_contour = detect_roosts(test_image)
-        cv2.namedWindow("Original image")
-        cv2.imshow("Original image", test_image.img)
-        for i in enumerate(roost_contour):
-            cv2.drawContours(
-                test_image.img,
-                roost_contour,
-                i[0],
-                (0, 0, 255),
-                1,
-            )
+# if __name__ == "__main__":
+#     test_image = testing()
+#     while test_image.active:
+#         roost_contour = detect_roosts(test_image)
+#         cv2.namedWindow("Original image")
+#         cv2.imshow("Original image", test_image.img)
+#         for i in enumerate(roost_contour):
+#             cv2.drawContours(
+#                 test_image.img,
+#                 roost_contour,
+#                 i[0],
+#                 (0, 0, 255),
+#                 1,
+#             )
 
-        key = cv2.waitKey(20)
-        if key == 27:
-            cv2.destroyAllWindows()
-            test_image.vc.release()
-            test_image.active = False
+#         key = cv2.waitKey(20)
+#         if key == 27:
+#             cv2.destroyAllWindows()
+#             test_image.vc.release()
+#             test_image.active = False
